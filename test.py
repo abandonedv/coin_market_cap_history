837e1528f60836039b0f9d318b4c3a9370ee5d10
Вадим Ильиных <vadimiiofficial@mail.ru>
2022-03-24 20:51:13 +0300

    added

---
+++ test.py
@@ -0,0 +1,10 @@
+# import requests
+# from bs4 import BeautifulSoup
+#
+# URL = "https://coinmarketcap.com/currencies/bitcoin/historical-data/"
+#
+# p = requests.get(URL, timeout=10)
+# h = p.text
+# soup = BeautifulSoup(h, "lxml")
+# s = soup.find_all("div")
+# print(t)
