c562c64bf93d0f73d2cb50e41b0a62c978c3d1ce
Вадим Ильиных <vadimiiofficial@mail.ru>
2022-03-26 13:09:58 +0300

    reading and writing to csv added

---
+++ read_and_write.py
@@ -0,0 +1,44 @@
+import pprint
+import json
+import os
+import csv
+import write_into_csv
+
+PATH = "/home/ludwig/Documents/GitHub/test_parsing/saved_json"
+
+
+class hist_data():
+    def __init__(self, name, day_data):
+        self.coin_parse = name
+        self.coin_time = day_data["timeClose"][:10]
+        self.coin_value = day_data["quote"]["close"]
+
+
+def get_js_from_file(file):
+    with open("saved_json/" + file, 'r') as f:
+        js = json.load(f)
+        return js
+
+
+def get_all_data(json):
+    history_of_coin = []
+    list_of_quotes = json["data"]["quotes"]
+    name_of_coin = json["data"]["name"]
+    for day_data in list_of_quotes:
+        # pprint.pprint(day_data)
+        history_of_coin.append(hist_data(name_of_coin, day_data))
+    return history_of_coin
+
+
+def main():
+    files = os.listdir(PATH)
+    write_into_csv.create()
+    for file in files:
+        js = get_js_from_file(file)
+        l = get_all_data(js)
+        write_into_csv.insert(l)
+        # db.insert...
+
+
+if __name__ == "__main__":
+    main()
